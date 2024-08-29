d9c20398318b3c61862627508125a6c05140a4df
triesap <137732411+triesap@users.noreply.github.com>
2024-08-29 09:55:39 +0000

    Edit `/models/location-gcs` adding a test suite to create and list saved location_gcs models using device geolocation

--- src/routes/(app)/models/location-gcs/+page.svelte
+++ src/routes/(app)/models/location-gcs/+page.svelte
@@ -2,103 +2,139 @@
     import { goto } from "$app/navigation";
     import { cl } from "$lib/client";
     import LayoutTrellis from "$lib/components/layout-trellis.svelte";
-    import { type LocationGcsFormFields } from "@radroots/client/src/capacitor/sql";
-    import { trellis } from "@radroots/svelte-lib";
+    import { type LocationGcs, type LocationGcsFormFields } from "@radroots/client";
+    import { trellis as Trellis } from "@radroots/svelte-lib";
     import { location_geohash } from "@radroots/utils";
+
+    let locations_all = $state<LocationGcs[]>([]);
+
+    $effect(() => { 
+        (async () => {
+           try {
+            const res = await cl.db.location_gcs_get({
+                list: [`all`],
+            });
+            if(typeof res !== `string`) locations_all = res;
+           } catch (e) { }
+        })();
+     });
 </script>
 
-<svelte:component this={LayoutTrellis}>
-    <svelte:component
-        this={trellis}
-        basis={{
-            args: {
-                layer: 1,
-                title: {
-                    value: `Location GCS`,
-                },
-                list: [
-                    {
-                        offset: {
-                            mod: {
-                                key: `caret-left`,
-                            },
-                        },
-                        touch: {
-                            label: {
-                                left: [
-                                    {
-                                        value: "Back",
-                                    },
-                                ],
-                            },
-                            callback: async () => {
-                                await goto(`/`);
-                            },
-                        },
-                    },
-                    {
-                        touch: {
-                            label: {
-                                left: [
-                                    {
-                                        value: `Read Logs`,
-                                        classes: `capitalize`,
-                                    },
-                                ],
-                            },
-                            callback: async () => {
-                                await cl.dialog.alert(JSON.stringify(cl.db.logs));
-                            },
-                        },
-                    },
-                    {
-                        touch: {
-                            label: {
-                                left: [
-                                    {
-                                        value: `Add Current Location`,
-                                        classes: `capitalize`,
-                                    },
-                                ],
-                            },
-                            callback: async () => {
-                                const pos = await cl.geo.current();
-                                console.log(JSON.stringify(pos,null,4), `pos`)
-                                if (pos) {
-                                    const { lat, lng } = pos;
-                                    const geohash = location_geohash(lat, lng);
-                                    const fields: LocationGcsFormFields = {
-                                        lat: lat.toString(),
-                                        lng: lng.toString(),
-                                        geohash
-                                    };
-                                    console.log(JSON.stringify(fields,null,4), `fields`)
-                                    const res = await cl.db.location_gcs_add(fields);
-                                    await cl.dialog.alert(JSON.stringify(res));
-                                }
-                            },
-                        },
-                    },
-                    {
-                        touch: {
-                            label: {
-                                left: [
-                                    {
-                                        value: `Read All`,
-                                        classes: `capitalize`,
-                                    },
-                                ],
-                            },
-                            callback: async () => {
-                                const res = await cl.db.location_gcs_get({
-                                    list: [`all`],
-                                });
-                                await cl.dialog.alert(JSON.stringify(res));
-                            },
-                        },
-                    },
-                ],
+<LayoutTrellis>
+    <Trellis 
+    basis={{
+        args: {
+            layer: 1,
+            title: {
+                value: `Location GCS`,
             },
-        }}
+            list: [
+                {
+                    offset: {
+                        mod: {
+                            key: `caret-left`,
+                        },
+                    },
+                    touch: {
+                        label: {
+                            left: [
+                                {
+                                    value: "Back",
+                                },
+                            ],
+                        },
+                        callback: async () => {
+                            await goto(`/`);
+                        },
+                    },
+                },
+                {
+                    touch: {
+                        label: {
+                            left: [
+                                {
+                                    value: `Add Current Location`,
+                                    classes: `capitalize`,
+                                },
+                            ],
+                        },
+                        callback: async () => {
+                            const loc_gcs = await cl.geo.current();
+                            if (loc_gcs && typeof loc_gcs !== `string`) {
+                                const loc_gcs_label = await cl.dialog.prompt({
+                                    title: `Geolocation Label`,
+                                    message: `What is the name of the location.`,
+                                    input_placeholder: `Enter location name`,
+                                });
+                                if(loc_gcs_label === false) return;
+
+
+                                const { lat, lng } = loc_gcs;
+                                const geohash = location_geohash(lat, lng);
+                                const fields: LocationGcsFormFields = {
+                                    lat: lat.toString(),
+                                    lng: lng.toString(),
+                                    geohash,
+                                    label: loc_gcs_label
+                                };
+                                const exe_res = await cl.db.location_gcs_add(fields);
+                                if(typeof exe_res !== `string` && `id` in exe_res) {
+                                    await cl.dialog.alert(`Added new location (${exe_res.id})`);
+                                } else if(typeof exe_res === `string` && exe_res === `*-location-gcs-geohash-unique`) {
+                                    await cl.dialog.alert(`This location has already been added.`);
+                                }
+                            } else if (loc_gcs && typeof loc_gcs === `string`) {
+                                const dcf_res = await cl.dialog.confirm(`Enable location permission is required.`);
+                                if (dcf_res) {
+                                    await cl.settings.open(
+                                        cl.platform === `ios`
+                                            ? { ios: { setting: `LocationServices` } }
+                                            : { android: { setting: `Location` } },
+                                    );
+                                }
+                            }
+                        },
+                    },
+                },
+                {
+                    touch: {
+                        label: {
+                            left: [
+                                {
+                                    value: `Read Saved Locations`,
+                                    classes: `capitalize`,
+                                },
+                            ],
+                        },
+                        callback: async () => {
+                            const res = await cl.db.location_gcs_get({
+                                list: [`all`],
+                            });
+                            if(typeof res !== `string`) locations_all = res;
+            
+                        },
+                    },
+                },
+            ],
+        },
+    }}
     />
-</svelte:component>
+    <div class={`flex flex-col justify-center items-center`}>
+        {#if locations_all.length > 0}
+            {#each locations_all as location}
+                <div class={`flex flex-col justify-center items-center`}>
+                    <pre
+                        class={`glyph font-sans font-[400] text-layer-0-glyph`}>{JSON.stringify(
+                            location,
+                            null,
+                            4,
+                        )}</pre>
+                </div>
+            {/each}
+        {:else}
+            <p class={`glyph font-sans font-[400] text-layer-0-glyph`}>
+                {"No locations saved"}
+            </p>
+        {/if}
+    </div>
+</LayoutTrellis>
